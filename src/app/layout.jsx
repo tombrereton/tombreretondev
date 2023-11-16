bfbfc71c030960026f5d651cdd02cbe3b4195aa4
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2023-11-16 17:23:23 +1100

    Fix share data

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,12 +5,12 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Tom Brereton',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Tom Brereton - Tinkerer, Creator, Software Engineer',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'Hi I’m Tom, a software engineer based in Sydney. I specialise in cloud native software development with some recent projects constructed using .NET, Terraform and React.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
