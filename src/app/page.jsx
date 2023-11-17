22f25b6650b4abbf19ca42037fd848a096d5cf20
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2023-11-17 13:36:00 +1100

    update new line

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -288,7 +288,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Designer, Creator, Software Engineer
+            Designer, Creator,<br/>Software Engineer
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             Hi I’m Tom, a software engineer based in Sydney. I specialise in cloud native software development with some recent projects
