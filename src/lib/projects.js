b3379146c37270cbcb53f8c85bc5bffe44125dbd
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2023-11-17 17:08:46 +1100

    Update projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -4,24 +4,27 @@ import logoCosmos from '@/images/logos/cosmos.svg'
 import logoHelioStream from '@/images/logos/helio-stream.svg'
 import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
 import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoSoundSort from '@/images/logos/soundsort.svg'
+import logoZenmatch from '@/images/logos/zenmatch.svg'
+import logoAmeliaAmes from '@/images/logos/ameliaames.svg'
 
 export const getProjects = () => [
   {
-    name: 'SoundSort',
-    description: 'A website to build your perfect Spotify playlist.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
+      name: 'SoundSort',
+      description: 'A website to build your perfect Spotify playlist.',
+    link: { href: 'http://soundsort.net', label: 'soundsort.net' },
+    logo: logoSoundSort,
   },
   {
-    name: 'Gem Island',
-    description: 'A match three game like Candy Crush.',
+      name: 'Zen Match',
+      description: 'A game like Candy Crush but more zen.',
     link: { href: '#', label: 'github.com' },
-    logo: logoAnimaginary,
+    logo: logoZenmatch,
   },
   {
-    name: 'Amelia Ames Landscape Architecture',
-    description: 'A marketing website for a landscape architect.',
+      name: 'Amelia Ames Landscape Architecture',
+      description: 'A marketing website for a landscape architect.',
     link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
+    logo: logoAmeliaAmes,
   }
 ]
