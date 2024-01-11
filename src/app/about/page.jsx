66df6abdc7823c3374eeaebf69b2d80cf6e46079
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2024-01-11 15:10:29 +1100

    feat: update about me page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,7 +9,7 @@ import {
   LinkedInIcon,
   TwitterIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/avatar.jpg'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -58,58 +58,37 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I’m Spencer Sharp. I live in New York City, where I design the
-            future.
+            A little bit about me
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-            <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
-            </p>
-            <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
-            </p>
-            <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
-            </p>
-            <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
-            </p>
+            <p>As a seasoned Senior/Lead Software Engineer, I've developed a keen expertise in .NET and Microsoft Azure. My passion lies in mastering static typed languages and crafting cloud-native applications. I'm always game for adopting new languages and exploring diverse cloud platforms, making versatility one of my strongest suits.</p>
+
+            <p>In my personal projects, I harness a variety of technologies, including React, JavaScript, Golang, .NET, and Terraform, each chosen to suit the specific needs of the project. My portfolio ranges from playlist generation websites to match-three games. My current focus is on developing a small event management website. These projects, hosted on platforms like Azure, Vercel, and Fly.io, represent my ongoing commitment to exploring and expanding the frontiers of software engineering.</p>
+
+            <p>In my blog, I dive deep into the realms of software design, particularly focusing on designing for testability. I firmly believe in the transformative power of tests in enhancing readability, robustness, and agility of software. My approach is pragmatic, not dogmatic; I advocate for using Test-Driven Development (TDD) where it brings real value, rather than following it blindly. My posts explore technologies that streamline testing and discuss innovative testing strategies, aiming to be a resource for both budding and seasoned developers.</p>
+
+            <p>When I'm not coding or blogging, you'll find me practicing Muay Thai, lifting weights, delving into UI and product design, or getting lost in a good book. My approach to life is much like my approach to software: disciplined, creative, and always striving for improvement.</p>
+
+            <p>What sets me apart? My knack for designing systems through tests, my excellent mentoring skills, and my product-based approach to software design. These qualities not only define my work but also shape how I engage with technology on a broader scale.</p>
+
+            <p>With this knowledge and experience, I'm on a quest to boost small businesses and solo dreamers, turning their 'what-ifs' into 'heck yes'!</p>
+
           </div>
         </div>
         <div className="lg:pl-20">
           <ul role="list">
-            <SocialLink href="#" icon={TwitterIcon}>
-              Follow on Twitter
-            </SocialLink>
-            <SocialLink href="#" icon={InstagramIcon} className="mt-4">
-              Follow on Instagram
-            </SocialLink>
-            <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+            <SocialLink href="https://github.com/tombrereton" icon={GitHubIcon} className="mt-4">
               Follow on GitHub
             </SocialLink>
-            <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+            <SocialLink href="https://www.linkedin.com/in/tbrereton/" icon={LinkedInIcon} className="mt-4">
               Follow on LinkedIn
             </SocialLink>
             <SocialLink
-              href="mailto:spencer@planetaria.tech"
+              href="mailto:mail@tombrereton.dev"
               icon={MailIcon}
               className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
             >
-              spencer@planetaria.tech
+              mail@tombrereton.dev
             </SocialLink>
           </ul>
         </div>
