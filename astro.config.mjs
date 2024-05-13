2f874c26eb78599cd47fc200a3032ebe613a79a3
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2024-05-13 12:57:47 +1000

    feat: add vercel analytics for static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,16 @@ import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
 import alpinejs from "@astrojs/alpinejs";
 
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.tombrereton.dev',
   integrations: [mdx(), sitemap(), react(), tailwind(), lottie(), alpinejs()],
+  output: 'static',
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    }
+  }),
 });
