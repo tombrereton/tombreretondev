2147e0a26f8dbfee72dc0ef9526ef0fb23dce151
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2024-05-15 14:51:33 +1000

    feat: update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Tom Brereton';
-export const SITE_DESCRIPTION = 'Software Engineer Extraordinaire';
+export const SITE_DESCRIPTION = 'Software Engineer | .NET, Go, React, Azure | Hire me';
