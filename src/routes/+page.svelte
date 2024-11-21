47eabfece6bb86a9e15a7b89dcaba91b47f213ce
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2024-11-21 23:58:51 +1100

    update landing for desktop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,50 +2,51 @@
 	import portrait from '$lib/assets/portrait.png';
 </script>
 
-<div class="gap-15 flex flex-col items-center gap-12 overflow-clip p-6">
-	<!-- <div class="bg-maroon absolute -left-4 top-16 -z-10 h-72 w-72 rounded filter "></div> -->
-	<!-- <div
-		class="animate-blob bg-maroon absolute -right-5 top-12 -z-10 h-72 w-72 rounded-full blur-3xl filter"
-	></div>
+<div
+	class="gap-15 flex flex-col items-center gap-12 overflow-clip p-6 lg:h-screen lg:justify-center lg:gap-32"
+>
+	<div class="gap-15 flex flex-col items-center gap-12 overflow-clip lg:max-w-5xl lg:flex-row">
+		<div
+			id="hero"
+			class="backlight relative flex flex-col items-center justify-center rounded bg-teal px-2 lg:px-6"
+		>
+			<h1
+				class="mt-6 text-center font-Gilroy-Black
+			text-7xl font-black leading-none tracking-tighter text-dark lg:text-9xl"
+			>
+				Tom Brereton
+			</h1>
+			<img src={portrait} alt="Tom Brereton" class="z-10 -mt-10 w-64 lg:-mt-16" />
+		</div>
+		<div id="landing-text" class="mx-1 max-w-lg text-2xl text-teal lg:text-3xl">
+			<p>
+				Hi! I’m Tom from <span class="font-Gilroy-Bold">Australia</span>. I’m a
+				<span class="font-Gilroy-Bold">Software Engineer</span>,
+				<span class="font-Gilroy-Bold">Team Lead</span> and
+				<span class="font-Gilroy-Bold">Cloud Architect</span>. In these roles my main tools are
+				<span class="font-Gilroy-Bold">C# & .NET</span>,
+				<span class="font-Gilroy-Bold">Microsoft Azure</span> and
+				<span class="font-Gilroy-Bold">Terraform</span> but I also enjoy Typescript, React & Svelte.
+				If you need help in these areas,
+				<span class="font-Gilroy-Bold">reach out below</span>.
+			</p>
+		</div>
+	</div>
 	<div
-		class="animate-blob bg-maroon absolute top-20 -z-10 h-72 w-72 rounded-full blur-3xl filter"
-	></div> -->
-	<div
-		id="hero"
-		class="backlight relative flex flex-col items-center justify-center rounded bg-teal px-2"
+		id="landing-nav"
+		class="lg flex w-full flex-col items-center gap-10 lg:flex-row lg:justify-center"
 	>
-		<h1
-			class="mt-6 text-center font-Gilroy-Black
-			text-7xl font-black leading-none tracking-tighter text-dark"
-		>
-			Tom Brereton
-		</h1>
-		<img src={portrait} alt="Tom Brereton" class="z-10 -mt-10 w-64" />
-	</div>
-	<div id="landing-text" class="mx-1 text-2xl text-teal">
-		<p>
-			Hi! I’m Tom from <span class="font-Gilroy-Bold">Australia</span>. I’m a
-			<span class="font-Gilroy-Bold">Software Engineer</span>,
-			<span class="font-Gilroy-Bold">Team Lead</span> and
-			<span class="font-Gilroy-Bold">Cloud Architect</span>. In these roles my main tools are
-			<span class="font-Gilroy-Bold">C# & .NET</span>,
-			<span class="font-Gilroy-Bold">Microsoft Azure</span> and
-			<span class="font-Gilroy-Bold">Terraform</span> but I also enjoy Typescript, React & Svelte.
-			If you need help in these areas, <span class="font-Gilroy-Bold">reach out below</span>.
-		</p>
-	</div>
-	<div id="landing-nav" class="flex w-full flex-col gap-10">
 		<a
-			class="rounded-3xl bg-flamingo pb-1 pt-3 text-center font-Gilroy-Bold text-5xl text-dark shadow-flamingo"
-			href="/contact">Hire Me</a
+			class="flex h-16 w-80 items-center justify-center rounded-3xl bg-flamingo pt-1 text-center font-Gilroy-Bold text-5xl text-dark shadow-flamingo"
+			href="/contact">Let's chat!</a
 		>
 		<a
-			class="rounded-3xl pb-2 pt-3 text-center font-Gilroy-SemiBold text-3xl text-teal outline outline-2"
+			class="flex h-16 w-80 items-center justify-center rounded-3xl pt-1 text-center font-Gilroy-Regular text-4xl text-teal outline outline-2"
 			href="/projects">See my projects</a
 		>
 		<a
-			class="rounded-3xl pb-2 pt-3 text-center font-Gilroy-SemiBold text-3xl text-teal outline outline-2"
-			href="/blog">My Blog</a
+			class="flex h-16 w-80 items-center justify-center rounded-3xl pt-1 text-center font-Gilroy-Regular text-4xl text-teal outline outline-2"
+			href="/blog">My blog</a
 		>
 	</div>
 </div>
